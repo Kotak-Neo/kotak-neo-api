73cc5c8c51324870bf06147a34d55de0043890b5
Supritha <supritha.n@digital.datamatics.com>
2023-06-12 13:05:50 +0530

    Added functionality for neo_fin_key optional parameter

---
+++ build/lib/neo_api_client/urls.py
@@ -0,0 +1,16 @@
+#############################################################################
+#                               WebSocket URLs
+#############################################################################
+WEBSOCKET_URL = "wss://lhsm.kotaksecurities.com"
+
+#############################################################################
+#                               UAT BASE URLs
+#############################################################################
+UAT_BASE_URL = "https://nsbxapi-gw.kotaksecurities.com/"
+PROD_BASE_URL = "https://gw-napi.kotaksecurities.com/"
+
+#############################################################################
+#                               PROD BASE URLs
+#############################################################################
+SESSION_UAT_BASE_URL = "https://nsbxapi.kotaksecurities.com/"
+SESSION_PROD_BASE_URL = "https://napi.kotaksecurities.com/"
